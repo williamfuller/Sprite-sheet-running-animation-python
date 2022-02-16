2fd1a114c6ee10291eb825ac00ef4997f7cf8609
William <williamfulle@gmail.com>
2022-02-16 17:33:15 +0100

    refactor:ceaned up main added utils file

--- code/main.py
+++ code/main.py
@@ -9,7 +9,7 @@ animation_display = pygame.display.set_mode((animation_window["width"],animation
 
 sprite_img = pygame.image.load(f"res/{sprite_sheet['name']}")
 sprite_info = SpriteInfoCalculator(sprite_img, sprite_sheet["animation_row"], sprite_sheet["frame_count"], sprite_sheet["row_count"])
-sprites_coordinates = sprite_info.get_sprite_coordinates()
+sprite_coordinates = sprite_info.get_sprite_coordinates()
 sprites = []
 
 for display_row in range (int(animation_window["height"] / sprite_info.sprite_height)): 
@@ -20,16 +20,15 @@ for display_row in range (int(animation_window["height"] / sprite_info.sprite_he
 running = True
 while running:
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False 
+	running = utils.is_animation_running(running)
 
 	animation_display.fill(animation_window["background_color"])
-
-	for sprite in sprites:
-		animation_frame_index = sprite.get_animation_frame_index(sprite_sheet["frame_count"])
-		sprite_pos = sprite.update_sprite_postion()
-		animation_display.blit(sprite_img, sprite_pos, sprites_coordinates[animation_frame_index])
+	animation_display = utils.generate_next_frame(animation_display, 
+												  animation_window["background_color"], 
+												  sprites, sprite_sheet, 
+												  sprite_img, 
+												  sprite_coordinates
+												)
 
 	pygame.display.update() 
 
--- code/utils.py
+++ code/utils.py
@@ -2,3 +2,19 @@
 This file contains a collection of utility
 functions
 """
+import pygame
+
+def is_animation_running(running):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+    return running
+
+def generate_next_frame(frame, background_color, sprites, sprite_sheet, sprite_img, sprite_coordinates):
+    frame.fill(background_color)
+
+    for sprite in sprites:
+        animation_frame_index = sprite.get_animation_frame_index(sprite_sheet["frame_count"])
+        sprite_pos = sprite.update_sprite_postion()
+        frame.blit(sprite_img, sprite_pos, sprite_coordinates[animation_frame_index])
+    return frame
